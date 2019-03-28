a0ec30140f8b5bafb31ab2802fe954ac85ec39f3
Tanvi Nerkar <tanvinerkar517@gmail.com>
2019-03-28 19:34:30 +0530

    Documented the code

--- algo-implement.py
+++ algo-implement.py
@@ -17,8 +17,7 @@ for i in range(n):
 file_o=open('op','w')
 file_o.close()
 file_o=open('op','a')
-# k=4
-# n=3
+
 from_nodes = [i for i in range(1,k+1)]
 to_nodes = [i for i in range(1,k+1)]
 
@@ -28,22 +27,17 @@ for i in range(1,k+1):
     ucap[i]=1
     vcap[i]=1
 
-# ucap = {1: 1, 2: 1, 3: 1} #u node capacities
-# vcap = {1: 1, 2: 1, 3: 1} #v node capacities
-
-""" inp=[[216, 439, 345,0],
-     [401, 211, 388,0],
-     [173, 315, 512,0],
-     [-100,-100,-100,-100]] """
 wts={}
 for i in range(1,k+1):
     for j in range(1,k+1):
         wts[(i,j)]=inp[i-1][j-1]
-""" wts = {(1, 1): 216, (1, 2): 439, (1, 3): 345,
-       (2, 1): 401, (2, 2): 211, (2, 3): 388,
-       (3, 1): 173, (3, 2): 315, (3, 3): 512}  """
 
 #just a convenience function to generate a dict of dicts
+
+#Converting the weights dictionary format 
+#from {(value1, value2): wt12, (value1, value3): wt13} 
+#to {value1: {value2: wt12, value3: wt13}}
+
 def create_wt_doubledict(from_nodes, to_nodes):
 
     wt = {}
@@ -85,13 +79,13 @@ def solve_wbm(from_nodes, to_nodes, wt):
     # The problem is solved using PuLP's choice of Solver
     prob.solve()
 
-    # The status of the solution is printed to the screen
+    # The status of the solution is written to the file
     file_o.write( "Status:"+ LpStatus[prob.status]+'\n')
     return(prob)
 
 
 def print_solution(prob):
-    # Each of the variables is printed with it's resolved optimum value
+    # Each of the variables is printed with its resolved optimum value
     for v in prob.variables():
         if v.varValue > 1e-3:
             n=v.name
@@ -117,6 +111,9 @@ print_solution(p)
 
 file_o.close()
 
+"""
+Reading the MWBM's results to create a list of the final room allotment.
+"""
 file_o=open('op','r')
 L=file_o.readlines()
 room_allot=[]
@@ -124,37 +121,58 @@ for i in range(1,n+1):
     room_allot.append((L[i][4],L[i][2]))
 file_o.close()
 
+"""
+Creating a transpose of the matrix inp.
+"""
 inp_transpose=[[0 for x in range(k)] for y in range(k)]
 for i in range(k):
     for j in range(k):
         inp_transpose[j][i]=inp[i][j]
 
+"""
+Sorting the room allocation list to get increasing order of room numbers.
+"""
 room_allot.sort()
-# print(len(room_allot))
+
+"""
+Setting the lower bound of each room's final value to the 2nd highest price bid (after the price bid by the student to whom the room has been allotted) for that room.
+"""
+
 bound=[]
 for i in range(n):
     if int(room_allot[i][1])<=n:
-        mx=inp_transpose[i].pop(int(room_allot[i][1])-1)
+        mx=inp_transpose[i].pop(int(room_allot[i][1])-1) #Removing the price which the person (to whom that room has been allotted) bid for that room
         mx_new=max(inp_transpose[i])
         bound.append((mx_new,mx))
-
+"""
+Setting the parameters for the linear optimization function.
+"""
 c=[1 for x in range(n)]
 A_eq =[[1 for x in range(n)]]
 b_eq =[1000]
-# bound=((216,401),(315,439),(388,512))
-
 res=scipy.optimize.linprog(c, A_eq=A_eq, b_eq=b_eq, bounds=bound, method='simplex')
-# print(res.x)
+
+"""
+Calculating the utility using the result of the linear optimization.
+Note: The solution returned by the linear optimization function is not the final solution as envy-freeness has not been checked for yet.
+"""
 
 utility=0.0
-
-# print(len(room_allot))
 
 for i in range(n):
     # if int(room_allot[i][1])<=n:
     utility+= (inp[int(room_allot[i][1])-1][i]) - (res.x)[i]
 
 avg_utility=utility/n
+"""
+By calculating the average utility, envy-freeness is guaranteed.
+It is assured that the envy-freeness condition is satisfied.
+"""
+
+"""
+Calculating the final price as:
+Final Price = Price bid for room by student to which it is assigned - Average Utility
+"""
 
 final_answer=[]
 for i in range(n):
